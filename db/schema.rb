442852c96b7c71e4f0dc512dee37a9ffa28c6d3e
Bernard Pietraga <bernard.pietraga@gmail.com>
2016-07-19 18:18:02 +0200

    remove trailing whitespace from schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,4 @@
 ActiveRecord::Schema.define(version: 20160719103419) do
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -19,5 +18,4 @@ ActiveRecord::Schema.define(version: 20160719103419) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 end
